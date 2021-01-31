53e443f5d267a2f685933b7d2ae859a8e15704e0
Gautam kumar jaiswal <42580621+thegautamkumarjaiswal@users.noreply.github.com>
2021-01-31 13:48:49 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,14 @@ import random
 
 def main():
   print("Keep it logically awesome.")
-
+  print("hey! whats up.")
+  
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   print(quotes[0])
+  print(quotes[2])
   
   last = 13
   rnd = random.randint(0, last)
